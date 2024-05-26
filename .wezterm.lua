01e8b9046810a126f773b798fadda156bb00d29e
Konstantin Fedotov <zenflak@gmail.com>
2024-05-26 22:20:04 +0900

    Font changed

--- .wezterm.lua
+++ .wezterm.lua
@@ -26,7 +26,7 @@ config.window_frame = {
 	-- Whatever font is selected here, it will have the
 	-- main font setting appended to it to pick up any
 	-- fallback fonts you may have used there.
-	font = wezterm.font({ family = "IosevkaTerm NFM", weight = "Regular" }),
+	font = wezterm.font({ family = "M+1Code Nerd Font", weight = "Regular" }),
 
 	-- The size of the font in the tab bar.
 	-- Default to 10.0 on Windows but 12.0 on other systems
